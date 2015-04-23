57aa95de3c6992788b4da7506809c7a69cf00f67
christopherwaldon <cw@kspcs.com>
2015-04-23 18:30:20 -0400

    Trying to reformat

--- dropdown.php
+++ dropdown.php
@@ -185,7 +185,7 @@ function dispatcher($newUni, $oldUni, $newDep, $oldDep, $newProf, $oldProf, $new
     elseif (changed_from_null($newSec, $oldSec)) {
     	$sql = "select * from resource where S_ID = $newSec;";
     	$result = mysql_query($sql);
-    	echo "<table id='output'><tr><td>Resources for section with ID = $newSec</td></tr>";
+    	echo "<table id='output'><tr><td>Resources for section with ID = $newSec</td><td></td><td></td></tr>";
     	echo '<tr><td>Description</td><td>Path</td><td>Upload Date</td></tr>';
     	while ($row = mysql_fetch_array($result)) {
     		echo "<tr value='{$row['ID']}><td>{$row['Description']}</td><td>{$row['Path']}</td><td>{$row['Upload_Date']}</td></tr>";
